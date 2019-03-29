7398e230d43917eda4e4c208fcae9871e161ec48
Nikita Loskutov <cnaize@gmail.com>
2019-03-29 21:38:19 +0400

    pointing to https://api.muzezone.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   coreBaseUrl: 'http://localhost:11311',
-  centerBaseUrl: 'https://mz-center.herokuapp.com',
+  centerBaseUrl: 'https://api.muzezone.com',
 };
